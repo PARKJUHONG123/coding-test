0aea0fa7f551ae0f371c2b6682755ed64d8a2198
PARKJUHONG123 <corallines@naver.com>
2022-07-03 20:26:26 +0900

    백준 2015 펜윅트리 추가

--- N2015.py
+++ N2015.py
@@ -22,3 +22,31 @@ for i in range(N):
     if target:
         answer += bisect.bisect_left(target, i)
 print(answer)
+
+
+
+'''
+tree = [0 for _ in range(N + 1)]
+
+def update(index, diff):
+    while index <= N:
+        tree[index] += diff
+        index += (index & -index)
+
+def sum(index):
+    ret = 0
+    while index > 0:
+        ret += tree[index]
+        index -= (index & -index)
+    return ret
+
+for index in range(N):
+    update(index + 1, arr[index])
+
+answer = 0
+for start in range(1, N + 1):
+    for end in range(start, N + 1):
+        if sum(end) - sum(start - 1) == K:
+            answer += 1
+print(answer)
+'''
